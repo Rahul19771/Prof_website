83521dc9df7b7111f592289fc450b3569429b588
Rahul19771 <rahulreddy19771@gmail.com>
2022-01-11 11:09:21 +0530

    auto scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -244,10 +244,10 @@
      * Portfolio details slider
      */
     new Swiper('.portfolio-details-slider', {
-      speed: 400,
+      speed: 1000,
       loop: true,
       autoplay: {
-        delay: 5000,
+        delay: 1000,
         disableOnInteraction: false
       },
       pagination: {
